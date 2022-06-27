711949e81f965686351c7f7a3fe6b93c37b8df3d
Stephen Hodges <stephen@shodges.io>
2022-06-27 02:40:41 -0400

    switch base layer to colemak

--- keyboards/bastardkb/charybdis/hybrid/keymaps/vial/keymap.c
+++ keyboards/bastardkb/charybdis/hybrid/keymaps/vial/keymap.c
@@ -54,6 +54,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 
 // clang-format off
 /** \brief QWERTY layout (3 rows, 10 columns). */
+/*
 #define LAYOUT_LAYER_BASE                                                                                                     \
     ___________________DEAD_HALF_ROW____________________,       ___________________DEAD_HALF_ROW____________________,         \
     XXXXXXX,   KC_Q,   KC_W,   KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,   KC_I,      KC_O,     KC_P,      XXXXXXX,   \
@@ -61,6 +62,16 @@ static uint16_t auto_pointer_layer_timer = 0;
     XXXXXXX,   KC_Z,   KC_X,   KC_C,      KC_V,      KC_B,      KC_N,      KC_M,   KC_COMM,   KC_DOT,   KC_SLSH,   XXXXXXX,   \
                                ESC_MED,   SPC_NAV,   TAB_FUN,   ENT_SYM,   BSP_NUM,                                           \
                                           XXXXXXX,   XXXXXXX,   XXXXXXX
+*/
+/** COLEMAK layout */
+#define LAYOUT_LAYER_BASE                                                                                                     \
+    ___________________DEAD_HALF_ROW____________________,       ___________________DEAD_HALF_ROW____________________,         \
+    XXXXXXX,   KC_Q,   KC_W,   KC_F,      KC_P,      KC_B,      KC_J,      KC_L,   KC_U,      KC_Y,     KC_QUOT,   XXXXXXX,   \
+    XXXXXXX,   KC_A,   KC_R,   KC_S,      KC_T,      KC_G,      KC_M,      KC_N,   KC_E,      KC_I,     KC_O,      XXXXXXX,   \
+    XXXXXXX,   KC_Z,   KC_X,   KC_C,      KC_D,      KC_V,      KC_K,      KC_H,   KC_COMM,   KC_DOT,   KC_SLSH,   XXXXXXX,   \
+                               ESC_MED,   SPC_NAV,   TAB_FUN,   ENT_SYM,   BSP_NUM,                                           \
+                                          XXXXXXX,   XXXXXXX,   XXXXXXX
+
 
 /** Convenience row shorthands. */
 #define ___________________DEAD_HALF_ROW____________________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
